c26262eed12e9f039a01ec06a4cae93a96bb8af4
Michael "Cal" Krug <wolfdaemon@murena.io>
2026-02-10 01:18:50 -0600

    feat: my attempt for the last of the basic enum exersizes

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -30,5 +30,8 @@ def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
-  # word_list.reduce { |accumulator, word|
+  word_list.reduce(Hash.new(0)) do |result, word|
+    result[word] += word.length
+    result
+  end
 end
